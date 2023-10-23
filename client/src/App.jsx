b358bd03a50d93157fdb660691f727812fb579af
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-10-23 20:35:52 +0500

    next13 ecommerce app

--- client/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div>helo world</div>;
-}
-export default App;
--- client/src/components/common/Page404/index.js
+++
@@ -1,6 +0,0 @@
-const Page404 = () => {
-  return (
-    <div className="page-404">The page are you looking for was not found.</div>
-  );
-};
-export default Page404;
