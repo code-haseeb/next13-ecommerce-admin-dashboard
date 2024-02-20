675b8394dfbbf1307e003b964a9521a24ab586f7
Abdul Haseeb <121640819+code-haseeb@users.noreply.github.com>
2024-02-20 15:06:56 +0500

    middleware definition

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,8 @@ export default authMiddleware({
   publicRoutes: ["/api/:path*"],
 });
 
+//In server-side web application frameworks, the term middleware is often more specifically used to refer to pre-built software components that can be added to the framework's request/response processing pipeline, to handle tasks such as database access.
+
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
